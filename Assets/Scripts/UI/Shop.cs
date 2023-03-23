ec20297df3da5fcd2625613ece9dfee9fb433eb5
choiminsun <backwonand@sookmyung.ac.kr>
2023-03-24 01:37:58 +0900

    프로젝트 이동

---
+++ Assets/Scripts/UI/Shop.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Shop : MonoBehaviour
+{
+    public Text text;
+
+    public void goToBack()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+    public void rightSide()
+    {
+        
+    }
+
+    public void leftSide()
+    {
+
+    }
+
+    public void selectOrBuy()
+    {
+
+    }
+    
+
+}
