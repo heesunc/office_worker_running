964eb28472293b22c27cc29a2ba9cf90cb5e1ff5
qkrdmstn1014 <qkrdmstn1258@naver.com>
2023-03-28 16:49:24 +0900

    Stage 정리
    Stage 생성 시 아이템을 빈 오브젝트의 자식으로 생성하여 정리

--- Assets/Scripts/Manager/StageGenerate.cs
+++ Assets/Scripts/Manager/StageGenerate.cs
@@ -14,11 +14,11 @@ public class StageGenerate : MonoBehaviour
     public GameObject postItPrefab;
     public GameObject coffeePrefab;
 
-    public GameObject moneyPrefab; //Items Prefabs
-    public GameObject fileStackPrefab;
-    public GameObject mailPrefab;
-    public GameObject postItPrefab;
-    public GameObject coffeePrefab;
+    public GameObject moneyParent; //Items Prefabs
+    public GameObject fileStackParent;
+    public GameObject mailParent;
+    public GameObject postItParent;
+    public GameObject coffeeParent;
 
     public float distance = 7.0f;
     public int stageIndex; //Stage selection
@@ -61,30 +61,34 @@ public class StageGenerate : MonoBehaviour
             {
                 if (mapData[i, j] == 1) //Create money
                 {
-                   GameObject money = Instantiate(moneyPrefab, new Vector3(i * distance, 1.6f, j * distance), Quaternion.identity);
+                    GameObject money = Instantiate(moneyPrefab, new Vector3(i * distance, 1.6f, j * distance), Quaternion.identity);
                     money.name = "(" + i + "," + j + ")";
-
+                    money.transform.parent = moneyParent.transform;
 
                 }
                 else if (mapData[i, j] == 2) //Create fileStack
                 {
                     GameObject fileStack = Instantiate(fileStackPrefab, new Vector3(i * distance, 1.5f, j * distance), Quaternion.Euler(-90.0f, 0, 0));
                     fileStack.name = "(" + i + "," + j + ")";
+                    fileStack.transform.parent = fileStackParent.transform;
                 }
                 else if (mapData[i, j] == 3) //Create mail
                 {
                     GameObject mail = Instantiate(mailPrefab, new Vector3(i * distance, 1.5f, j * distance), Quaternion.Euler(0, 90.0f, 0));
                     mail.name = "(" + i + "," + j + ")";
+                    mail.transform.parent = mailParent.transform;
                 }
                 else if (mapData[i, j] == 4) //Create postIt
                 {
                     GameObject postIt = Instantiate(postItPrefab, new Vector3(i * distance, 1.2f, j * distance), Quaternion.Euler(0, 90.0f, 0));
                     postIt.name = "(" + i + "," + j + ")";
+                    postIt.transform.parent = postItParent.transform;
                 }
                 else if (mapData[i, j] == 5) //Create Coffee
                 {
                     GameObject coffee = Instantiate(coffeePrefab, new Vector3(i * distance, 0.7f, j * distance), Quaternion.Euler(-90.0f, 0, 0));
                     coffee.name = "(" + i + "," + j + ")";
+                    coffee.transform.parent = coffeeParent.transform;
                 }
                 else if(mapData[i, j] == 6) //move playerPos
                 {
