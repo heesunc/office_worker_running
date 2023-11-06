d070f6db55b808cd5071e33b435173050013b7d4
choiminsun <backwonand@sookmyung.ac.kr>
2023-11-06 14:05:57 +0900

    swipe Jump 수정

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -38,8 +38,10 @@ public class Player : MonoBehaviour
 
     public void Jump()
     {
+        Debug.Log("함수 시행");
         if (!isJump)
         {
+            Debug.Log("is Jump 정상");
             isJump = true;
             int direction = getMove();
             playerSound.SoundPlay("Jump");
@@ -139,6 +141,8 @@ public class Player : MonoBehaviour
         speedSet();
 
         step();
+
+        Debug.Log("swipe:" + Option.getController());
     }
 
     // Update is called once per frame
